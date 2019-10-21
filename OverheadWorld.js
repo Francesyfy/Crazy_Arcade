8444c97bb34cba24fe0bb6b18efd728ea9d261ba
Jessie-Zhou <jz2831@nyu.edu>
2019-10-21 17:40:42 -0400

    add bomb map

--- OverheadWorld.js
+++ OverheadWorld.js
@@ -6,6 +6,9 @@ function OverheadWorld(params) {
   // store our tile map
   this.tileMap = params.tileMap
 
+  // store the bomb map to track if a tile is bombed
+  this.bombMap = generateBombMap(params.tileMap)
+
   // store the folder in which all of our tiles are stored
   this.tileFolder = params.tileFolder + "/" + params.map
   
@@ -52,6 +55,14 @@ function OverheadWorld(params) {
     }
   }
 
+  this.displayBlocksByRow = function(row) {
+    for (var col = 0; col < this.tileMap[row].length; col += 1) {
+      if (this.tileMap[row][col] != 0){
+        image(this.tileLibrary[ this.tileMap[row][col] ], col*this.tileSize, (row - 0.4)*this.tileSize, this.tileSize, this.tileSize*1.4);
+      }
+    }
+  }
+
   // get a tile based on a screen x,y position
   this.getTile = function(x, y) {
     // convert the x & y position into a grid position
@@ -76,4 +87,17 @@ function OverheadWorld(params) {
     // otherwise return false
     return false;
   }
+}
+
+// generateBombMap: generate a map
+// to track if the tiles are bombed
+// later in the game
+function generateBombMap (tileMap) {
+  var row = tileMap.length
+  var col = tileMap[0].length    
+  var bombMap = Array(row)
+  for(var i = 0; i < row; i++){
+    bombMap[i] = Array(col).fill(0)
+  }
+  return bombMap
 }
