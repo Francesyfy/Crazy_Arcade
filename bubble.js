c1d2cd97de4c1017f897a1fdd0a3683748860500
Francesyfy <fy576@nyu.edu>
2020-11-26 14:23:24 +0800

    final version

--- bubble.js
+++ bubble.js
@@ -34,15 +34,21 @@
 // }
 
 class Bubble {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        
+    constructor(r, c, world, players) {
+        this.row = r
+        this.col = c
+        this.world = world
+        this.players = players
+
+        this.x = c * 50;
+        this.y = r * 50;
+
         this.state = 0;
         this.bounceTimes = 0;
-        
+
         this.currentFrame_Bubble = 0;
-        this.currentFrame_expBubble = 0;
+        this.currentFrame_expBubble_center = 0;
+        this.currentFrame_expBubble_surrounding = 0;
     }
 
     display() {
@@ -52,28 +58,91 @@ class Bubble {
             if (frameCount % 10 == 0) {
                 this.currentFrame_Bubble++;
             }
-    
+
             if (this.currentFrame_Bubble >= bubbleGraphics.length) {
                 this.currentFrame_Bubble = 0;
                 this.bounceTimes++;
             }
-            
+
             // the bubble bounces for 6 times, then explodes
             if (this.bounceTimes >= 6) {
                 this.state = 1
             }
-        } 
-        
+        }
+
         // explode
         else if (this.state == 1) {
-            image(expBubbleGraphics[this.currentFrame_expBubble], this.x, this.y);
+
+            // check if the players are within its explode range
+            var x = this.x
+            var y = this.y
+            for (var i = 0; i < players.length; i++) {
+                var x_p = this.players[i].x + 25
+                var y_p = this.players[i].y + 30
+
+                if ((x_p>x && x_p<x+50 && y_p>y-50 && y_p<y+100) || 
+                (x_p>x-50 && x_p<x+100 && y_p>y && y_p<y+50)) {
+                    this.players[i].lives -= 1
+                }
+                console.log(this.players[i].lives)
+
+            }
+
+            // adjust the lifeMap of the overhead world
+            // affect itself, up, down, left, right
+            var r = this.row
+            var c = this.col
+            var lifeMap = this.world.lifeMap
+            lifeMap[r][c] -= 1
+            if(r > 0){
+                lifeMap[r-1][c] -= 1
+            }
+            if (r < lifeMap.length - 1){
+                lifeMap[r+1][c] -= 1
+            }
+            if (c > 0){
+                lifeMap[r][c-1] -= 1
+            }
+            if (c < lifeMap[0].length - 1){
+                lifeMap[r][c+1] -= 1
+            }
+
+
+            image(expBubbleGraphics[this.currentFrame_expBubble_center], this.x, this.y, 50, 50);
             if (frameCount % 10 == 0) {
-                this.currentFrame_expBubble++;
+                this.currentFrame_expBubble_center++;
             }
-    
-            if (this.currentFrame_expBubble >= expBubbleGraphics.length) {
-                this.currentFrame_expBubble = 0;
+
+            if (this.currentFrame_expBubble_center >= expBubbleGraphics.length) {
+                this.currentFrame_expBubble_center = 0;
             }
+
+            image(expBubbleGraphicsLeft[this.currentFrame_expBubble_surrounding], this.x - 50, this.y, 50, 50);
+            if (frameCount % 10 == 0) {
+                this.currentFrame_expBubble_surrounding++;
+            }
+
+            image(expBubbleGraphicsRight[this.currentFrame_expBubble_surrounding], this.x + 50, this.y, 50, 50);
+            if (frameCount % 10 == 0) {
+                this.currentFrame_expBubble_surrounding++;
+            }
+
+            image(expBubbleGraphicsUp[this.currentFrame_expBubble_surrounding], this.x, this.y - 50, 50, 50);
+            if (frameCount % 10 == 0) {
+                this.currentFrame_expBubble_surrounding++;
+            }
+
+            image(expBubbleGraphicsDown[this.currentFrame_expBubble_surrounding], this.x, this.y + 50, 50, 50);
+            if (frameCount % 10 == 0) {
+                this.currentFrame_expBubble_surrounding++;
+            }
+
+            if (this.currentFrame_expBubble_surrounding >= expBubbleGraphicsLeft.length) {
+                this.currentFrame_expBubble_surrounding = 0;
+            }
+
+            // update bubbleMap
+            this.world.bubbleMap[this.row][this.col] = 0;
         }
     }
 }
--- sketch.js
+++ sketch.js
@@ -14,6 +14,10 @@ var players = []
 var bubbles = []
 var bubbleGraphics = []
 var expBubbleGraphics = []
+let expBubbleGraphicsLeft = [];
+let expBubbleGraphicsRight = [];
+let expBubbleGraphicsUp = [];
+let expBubbleGraphicsDown = [];
 
 // handle the tile loading and creating our player object in preload before the game can start
 function preload() {
@@ -21,11 +25,11 @@ function preload() {
   mapDesert = new Map('desert')
   mapUnderwater = new Map('underwater')
 
-  theWorld = new OverheadWorld(mapTown)
+  theWorld = new OverheadWorld(mapUnderwater)
 
   // players
-  // Boz: awsd - move; g - bubble
-  // Lodumani: arrows - move; m - bubble
+  // Boz: awsd - move; r - bubble
+  // Lodumani: jikl - move; p - bubble
   players.push(new Player(0, 0, "Boz", theWorld))
   players.push(new Player(700, 580, "Lodumani", theWorld))
 
@@ -37,32 +41,70 @@ function preload() {
   }
 
   for (let i = 1; i <= 4; i++) {
-      let filename = str(i) + ".png";
-      expBubbleGraphics.push(loadImage("bubble/explode/center/" + filename));
+    let filename = str(i) + ".png";
+    expBubbleGraphics.push(loadImage("bubble/explode/center/" + filename));
+  }
+
+  for (let i = 0; i <= 13; i++) {
+    let filename = i + ".png";
+    expBubbleGraphicsLeft.push(loadImage("bubble/explode/left/" + filename));
+  }
+
+  for (let i = 0; i <= 13; i++) {
+    let filename = i + ".png";
+    expBubbleGraphicsRight.push(loadImage("bubble/explode/right/" + filename));
+  }
+
+    for (let i = 0; i <= 13; i++) {
+        let filename = i + ".png";
+        expBubbleGraphicsUp.push(loadImage("bubble/explode/up/" + filename));
+    }
+
+  for (let i = 0; i <= 13; i++) {
+    let filename = i + ".png";
+    expBubbleGraphicsDown.push(loadImage("bubble/explode/down/" + filename));
   }
 }
 
 function setup() {
-  createCanvas(750,650)
+  createCanvas(750, 650)
 }
 
 function draw() {
   theWorld.displayGround()
-  //theWorld.displayBlocks()
+  theWorld.refreshTileMap()
 
   for (var i = 0; i < 2; i++) {
-    // click mouse, the character dies or reborns
+    // click mouse, the character reborns
     if (mouseIsPressed) {
-      if (players[i].state == 1)
+      if (players[i].state == 3) {
+        players[i].state = 0
+      }
+    }
+
+    // lives <= 0, character dies
+    if (players[i].lives <= 0){
       players[i].state = 2
-      else if (players[i].state == 3)
-      players[i].state = 0
-    }    
+      players[i].lives = 90
+    }
 
     //update occupied rows
     var r = players[i].row()
     theWorld.rows[r].occupied = 1
     theWorld.rows[r].players.push(players[i])
+  }
+
+  for (var i = 0; i < bubbles.length; i++) {
+    if (bubbles[i].currentFrame_expBubble_surrounding >= 12) {
+      bubbles.splice(i, 1);
+    }
+  }
+
+  for (var i = 0; i < bubbles.length; i++) {
+    //update occupied rows
+    var r = bubbles[i].row
+    theWorld.rows[r].occupied = 1
+    theWorld.rows[r].bubbles.push(bubbles[i])
   }
 
 
@@ -72,76 +114,93 @@ function draw() {
 
   // first and last row that is occupied
   var fOR = ors[0]
-  var lOR = ors[ors.length-1]
+  var lOR = ors[ors.length - 1]
 
   // display the first parts
-  if (fOR != 0){
+  if (fOR != 0) {
     theWorld.displayBlocksByRows(0, fOR)
   }
 
   // display the middle parts
-  for (var i = 0; i < ors.length - 1; i++){
+  for (var i = 0; i < ors.length - 1; i++) {
     // display the objects first
     var p = theWorld.rows[ors[i]].players
-    for (var j = 0; j < p.length; j++){
-      if (p[j].state == 0) {
-        p[j].startDisplay()
-      }
-      else if (p[j].state == 1) {
-        p[j].move()
-        p[j].display()
-  
-      } 
-      else if (p[j].state == 2) {
-        p[j].dieDisplay()
+    var b = theWorld.rows[ors[i]].bubbles
+
+    if (p.length > 0) {
+      for (var j = 0; j < p.length; j++) {
+        if (p[j].state == 0) {
+          p[j].startDisplay()
+        }
+        else if (p[j].state == 1) {
+          p[j].move()
+          p[j].display()
+        }
+        else if (p[j].state == 2) {
+          p[j].dieDisplay()
+        }
       }
     }
+
+    if (b.length > 0) {
+      for (var j = 0; j < b.length; j++) {
+        b[j].display()
+      }
+    }
+    
+
     // display the rows between i and i+1
-    theWorld.displayBlocksByRows(ors[i],ors[i+1])
+    theWorld.displayBlocksByRows(ors[i], ors[i + 1])
   }
 
   // display the objects on the last occupied row
   var lastP = theWorld.rows[lOR].players
-  for (var i = 0; i < lastP.length; i++){
-    if (lastP[i].state == 0) {
-      lastP[i].startDisplay()
-    }
-    else if (lastP[i].state == 1) {
-      lastP[i].move()
-      lastP[i].display()
-
-    } 
-    else if (lastP[i].state == 2) {
-      lastP[i].dieDisplay()
+  var lastB = theWorld.rows[lOR].bubbles
+  if (lastP.length > 0) {
+    for (var i = 0; i < lastP.length; i++) {
+      if (lastP[i].state == 0) {
+        lastP[i].startDisplay()
+      }
+      else if (lastP[i].state == 1) {
+        lastP[i].move()
+        lastP[i].display()
+      }
+      else if (lastP[i].state == 2) {
+        lastP[i].dieDisplay()
+      }
     }
   }
+
+  if (lastB.length > 0) {
+    for (var i = 0; i < lastB.length; i++) {
+      lastB[i].display()
+    }
+  }
+
 
   // display the last parts after the last occupied row
-  if (lOR != theWorld.length-1){
+  if (lOR != theWorld.length - 1) {
     theWorld.displayBlocksByRows(lOR, theWorld.length)
   }
-  
 
-  for (var i = 0; i < theWorld.rows.length; i++){
+
+  for (var i = 0; i < theWorld.rows.length; i++) {
     theWorld.rows[i] = {
       occupied: 0,
       players: [],
       bubbles: []
     }
   }
-
-
-  for (var i = 0; i < bubbles.length; i++) {
-    bubbles[i].display()
-  }
 }
 
 function keyPressed() {
-  if (key == 'g' || key == "G") {
-    bubbles.push(new Bubble(players[0].x+5, players[0].y+20))
+  if (key == 'r' || key == "R") {
+    bubbles.push(new Bubble(players[0].row(), players[0].col(), theWorld, players))
+    theWorld.bubbleMap[players[0].row()][players[0].col()] = 1
   }
-  if (key == 'm' || key == "M") {
-    bubbles.push(new Bubble(players[1].x+5, players[1].y+20))
+  if (key == 'p' || key == "P") {
+    bubbles.push(new Bubble(players[1].row(), players[1].col(), theWorld, players))
+    theWorld.bubbleMap[players[1].row()][players[1].col()] = 1
   }
 }
 
@@ -153,13 +212,13 @@ function keyPressed() {
   each mode has it own tile arrangements
 */
 class Map {
-  constructor(map){
+  constructor(map) {
     this.map = map
     this.tileSize = 50
     this.tileFolder = 'tiles'
     this.numTiles = 4
-    this.solidTiles = {1:true, 2:true, 3:true}
-    if (this.map == 'town'){
+    this.solidTiles = { 1: true, 2: true, 3: true }
+    if (this.map == 'town') {
       this.tileMap = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 2, 0, 0],
@@ -175,8 +234,23 @@ class Map {
         [0, 0, 2, 2, 0, 3, 0, 2, 0, 2, 0, 1, 2, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       ]
+      this.lifeMap = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 60, 180, 60, 120, 60, 120, 60, 180, 60, 120, 120, 0, 0],
+        [0, 60, 60, 120, 180, 120, 60, 60, 120, 180, 60, 120, 60, 60, 0],
+        [0, 60, 0, 120, 0, 180, 0, 180, 0, 60, 0, 180, 0, 60, 0],
+        [0, 120, 60, 180, 60, 120, 60, 120, 60, 180, 60, 120, 120, 60, 0],
+        [0, 180, 0, 180, 0, 60, 0, 60, 0, 120, 0, 60, 0, 120, 0],
+        [0, 120, 180, 60, 120, 120, 60, 120, 60, 180, 60, 180, 120, 60, 0],
+        [0, 120, 0, 120, 0, 180, 0, 120, 0, 120, 0, 60, 0, 120, 0],
+        [0, 180, 180, 60, 180, 60, 120, 180, 60, 120, 60, 180, 120, 60, 0],
+        [0, 60, 0, 60, 0, 120, 0, 60, 0, 60, 0, 180, 0, 60, 0],
+        [0, 60, 60, 60, 120, 180, 60, 180, 60, 120, 180, 120, 60, 60, 0],
+        [0, 0, 120, 120, 0, 180, 0, 120, 0, 120, 0, 60, 120, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
     }
-    else if (this.map == 'desert'){
+    else if (this.map == 'desert') {
       this.tileMap = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 2, 0, 0],
@@ -192,8 +266,28 @@ class Map {
         [0, 0, 2, 2, 0, 3, 0, 2, 0, 2, 0, 1, 2, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       ]
+      this.lifeMap = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 2, 0, 0],
+        [0, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 0],
+        [0, 1, 0, 2, 1, 3, 0, 3, 0, 1, 0, 1, 0, 1, 0],
+        [0, 1, 1, 3, 2, 1, 3, 1, 1, 1, 1, 2, 3, 2, 0],
+        [0, 1, 0, 3, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0],
+        [0, 2, 1, 2, 1, 3, 1, 1, 3, 2, 1, 3, 1, 1, 0],
+        [0, 3, 0, 1, 0, 1, 0, 2, 0, 1, 0, 2, 0, 2, 0],
+        [0, 1, 3, 1, 2, 1, 1, 3, 1, 2, 1, 3, 1, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 3, 0, 1, 0, 1, 0],
+        [0, 1, 1, 2, 1, 2, 1, 3, 1, 2, 1, 3, 1, 1, 0],
+        [0, 0, 2, 2, 0, 3, 0, 2, 0, 2, 0, 1, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ] 
+      for (var i = 0; i < this.lifeMap.length; i++) {
+        for (var j = 0; j < this.lifeMap[i].length; j++) {
+          this.lifeMap[i][j] *= 30
+        }
+      }
     }
-    else if (this.map == 'underwater'){
+    else if (this.map == 'underwater') {
       this.tileMap = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 2, 0, 0],
@@ -209,6 +303,26 @@ class Map {
         [0, 0, 2, 2, 0, 3, 0, 2, 0, 2, 0, 1, 2, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       ]
+      this.lifeMap = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 3, 1, 2, 1, 2, 1, 3, 1, 2, 2, 0, 0],
+        [0, 1, 2, 1, 2, 1, 1, 3, 1, 2, 1, 1, 1, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 3, 0, 1, 0],
+        [0, 1, 3, 1, 2, 1, 3, 2, 1, 1, 1, 2, 1, 2, 0],
+        [0, 2, 0, 2, 0, 3, 0, 3, 0, 1, 0, 2, 0, 1, 0],
+        [0, 1, 3, 1, 2, 1, 3, 1, 1, 1, 2, 1, 3, 1, 0],
+        [0, 1, 0, 3, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2, 0],
+        [0, 2, 1, 2, 1, 1, 1, 2, 1, 2, 2, 3, 3, 1, 0],
+        [0, 1, 0, 1, 0, 1, 0, 2, 0, 1, 0, 2, 0, 1, 0],
+        [0, 1, 1, 3, 1, 3, 1, 1, 1, 3, 1, 1, 1, 1, 0],
+        [0, 0, 2, 2, 0, 3, 0, 2, 0, 2, 0, 1, 2, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
+      for (var i = 0; i < this.lifeMap.length; i++) {
+        for (var j = 0; j < this.lifeMap[i].length; j++) {
+          this.lifeMap[i][j] *= 30
+        }
+      }
     }
   }
 }
